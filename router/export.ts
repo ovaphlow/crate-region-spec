f08bb12756b6fadf378bb6998f33bbbd6c2c991d
ovaphlow <ovaphlow@live.com>
2024-12-11 12:41:18 +0800

    导出xlsx

--- router/export.ts
+++ router/export.ts
@@ -1,13 +1,29 @@
 import { Context, Hono } from 'hono'
+import * as XLSX from 'xlsx'
 
 const app = new Hono()
 	.post('/excel', exportExcel)
 
 export default app
 
-export async function exportExcel(c: Context) {
-	const {columns, data} = await c.req.json()
-	console.info(columns, 'columns')
-	console.info(data, 'data')
-	return c.text('Export Excel')
+/**
+ * Handles the export of data to an Excel file.
+ * 
+ * @param {Context} c - The context object containing the request and response.
+ */
+async function exportExcel(c: Context) {
+	const { columns, data } = await c.req.json()
+
+	const wb = XLSX.utils.book_new()
+	const ws = XLSX.utils.aoa_to_sheet([
+		columns,
+		...data
+	])
+	XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1')
+
+	const buf = XLSX.write(wb, { type: 'buffer', bookType: 'xlsx' })
+
+	c.header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+	c.header('Content-Disposition', 'attachment; filename="export.xlsx"')
+	return c.body(buf)
 }
